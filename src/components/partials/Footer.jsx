845a7abd434159c1e4a7c44d73a4d70644b58ef3
Maximzp83 <maximzp83@gmail.com>
2018-01-22 00:38:09 +0200

    first homework ready

---
+++ src/components/partials/Footer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+
+
+
+
+function Footer(props) {
+    return (
+      <footer className="footer">
+        <div className="container">
+        	<p>footer content</p>
+        </div>
+      </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/partials/Header.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+
+
+
+
+function Header(props)  {
+  
+    return (
+      <header className="header">
+        <div className="container">
+        	<h1>Test React App</h1>
+        </div>
+      </header>
+    );
+  
+}
+
+export default Header;
